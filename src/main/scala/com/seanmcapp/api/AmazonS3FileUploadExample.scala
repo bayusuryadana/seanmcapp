cc76be216a21db806ed588f174e0d6e4a3915d66
Suryadana, Bayu (Agoda) <bayu.suryadana@agoda.com>
2018-11-08 16:39:21 +0700

    flow from DB to HDrive

--- src/main/scala/com/seanmcapp/api/AmazonS3FileUploadExample.scala
+++ src/main/scala/com/seanmcapp/api/AmazonS3FileUploadExample.scala
@@ -2,25 +2,38 @@ package com.seanmcapp.api
 
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
 import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
-import java.io.File
+import java.net.URL
 
+import com.amazonaws.services.s3.model.ObjectMetadata
 import com.seanmcapp.config.AWSConf
+import com.seanmcapp.repository.mongodb.PhotoRepoImpl
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object AmazonS3FileUploadExample {
 
-  def ehek(): String = {
-    val fileToUpload = new File("ehek.jpg")
-    val filename = "seanmcapp/wow.jpg"
-
-    val awsConf = AWSConf()
-    val credentials = new BasicAWSCredentials(awsConf.access, awsConf.secret)
-    val amazonS3Client = AmazonS3ClientBuilder
-      .standard
-      .withCredentials(new AWSStaticCredentialsProvider(credentials))
-      .withRegion(awsConf.region)
-      .build
-    amazonS3Client.putObject(awsConf.bucket, filename, fileToUpload)
-    "135"
+  def ehek(): Future[String] = {
+    for {
+      photoRepo <- new PhotoRepoImpl().getAll()
+    } yield {
+      val metadata = new ObjectMetadata()
+      metadata.setContentType("image/jpeg")
+      println("content-type => " + metadata.getContentType)
+      val awsConf = AWSConf()
+      val credentials = new BasicAWSCredentials(awsConf.access, awsConf.secret)
+      val amazonS3Client = AmazonS3ClientBuilder
+        .standard
+        .withCredentials(new AWSStaticCredentialsProvider(credentials))
+        .withRegion(awsConf.region)
+        .build
+      photoRepo.map { photo =>
+        val file = new URL(photo.thumbnailSrc).openStream()
+        val filename = "seanmcapp/" + photo.id + ".jpg"
+        amazonS3Client.putObject(awsConf.bucket, filename, file, metadata)
+        println("[DONE] " + photo.id)
+      }
+      "100"
+    }
   }
-
 }
