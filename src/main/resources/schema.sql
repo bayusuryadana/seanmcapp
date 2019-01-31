8391c69f316596021fbb924cf1ff8302394ddae0
bayu suryadana <bayusuryadana@gmail.com>
2019-02-01 00:53:35 +0700

    add table and populate heroes

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -25,3 +25,42 @@ create table votes (
 	rating smallint not null,
 	primary key (customers_id, photos_id)
 );
+
+create table heroes (
+	id SMALLINT primary key,
+	localized_name varchar(32) not null,
+	primary_attr varchar(3) not null,
+	attack_type varchar(6) not null
+);
+
+create table players (
+	id int primary key,
+	realname varchar(32) not null,
+	avatarfull varchar(2083),
+	personaname varchar(64) not null,
+	mmr_estimate int not null
+);
+
+create table peers (
+	players_id int references players(id),
+	peer_players_id int references players(id),
+	win int not null,
+	games int not null,
+	primary key (players_id, peer_players_id)
+)
+
+create table matches (
+	id int primary key,
+	players_id int references players(id),
+	player_slot int not null,
+	radiant_win bit not null,
+	duration integer not null,
+	game_mode integer not null,
+	lobby_type integer not null,
+	hero_id integer not null,
+	start_time integer not null,
+	kills integer not null,
+	deaths integer not null,
+	assists integer not null,
+	skill integer not null
+);
