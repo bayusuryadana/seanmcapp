9af10e61cf819cf101de695378724ad3194d8c83
Irfan Nur Afif <irfannafif@gmail.com>
2019-07-31 15:44:14 +0800

    move scheduler recipient to config (#35)
    * move scheduler recipient to conf
    
    * change scheduler recipient to env variable
    
    * fix split in schedulerconf
    
    * change instanceOf to toSeq

---
+++ src/main/scala/com/seanmcapp/config/SchedulerConf.scala
@@ -0,0 +1,22 @@
+package com.seanmcapp.config
+
+import com.typesafe.config.{Config, ConfigFactory}
+import config.Configuration
+
+import scala.util.Try
+
+case class SchedulerConf(amartha: Seq[Long], igrow: Seq[Long])
+
+object SchedulerConf extends Configuration[SchedulerConf] {
+
+  override val prefix: String = "scheduler"
+
+  def apply(): SchedulerConf = apply(ConfigFactory.load)
+
+  override def fromSubConfig(c: Config): SchedulerConf = {
+    SchedulerConf(Try(c.getString("amartha").split(",").map(_.toLong).toSeq)
+      .getOrElse(Seq.empty[Long]),
+      Try(c.getString("igrow").split(",").map(_.toLong).toSeq).getOrElse(Seq.empty[Long])
+    )
+  }
+}
