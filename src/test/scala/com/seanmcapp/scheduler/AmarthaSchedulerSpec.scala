5ce1368b07b52c278caa4f048c40b4a7996c7799
bayu suryadana <bayusuryadana@gmail.com>
2019-08-23 00:35:43 +0800

    fix mockito AmarthaScheduler

--- src/test/scala/com/seanmcapp/scheduler/AmarthaSchedulerSpec.scala
+++ src/test/scala/com/seanmcapp/scheduler/AmarthaSchedulerSpec.scala
@@ -3,7 +3,9 @@ package com.seanmcapp.scheduler
 import com.seanmcapp.SchedulerForTest
 import com.seanmcapp.mock.requestbuilder.TelegramRequestBuilderMock
 import com.seanmcapp.util.parser.decoder.{AmarthaMarketplaceData, AmarthaMarketplaceItem}
-import org.mockito.Mockito.when
+import org.mockito.ArgumentMatchers
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito._
 import org.scalatest.{AsyncWordSpec, Matchers}
 
 import scala.io.Source
@@ -16,15 +18,12 @@ class AmarthaSchedulerSpec extends AsyncWordSpec with Matchers with SchedulerFor
     val amartha = new AmarthaScheduler(startTime, None, http) with TelegramRequestBuilderMock {
       override def rerun(response: AmarthaMarketplaceData): Unit = {}
     }
-    val authResponse = Source.fromResource("scheduler/amartha/auth_response.json").mkString
-    val postData = Some(s"""{"username": "","password": ""}""")
-    val authHeaders = Some(Map("Content-Type" -> "application/json"))
-    val timeout = Some((15000, 300000))
-    when(http.sendRequest(amarthaBaseUrl + "/auth", postData, authHeaders, timeout)).thenReturn(authResponse)
 
-    val headers = Some(Map("x-access-token" -> "access_token"))
+    val authResponse = Source.fromResource("scheduler/amartha/auth_response.json").mkString
+    when(http.sendRequest(ArgumentMatchers.eq(amarthaBaseUrl + "/auth"), any(), any(), any())).thenReturn(authResponse)
+
     val marketplaceResponse = Source.fromResource("scheduler/amartha/marketplace_response.json").mkString
-   when(http.sendRequest(amarthaBaseUrl + "/marketplace", headers = headers, timeout = timeout)).thenReturn(marketplaceResponse)
+    when(http.sendRequest(ArgumentMatchers.eq(amarthaBaseUrl + "/marketplace"), any(), any(), any())).thenReturn(marketplaceResponse)
 
     amartha.task shouldBe List(AmarthaMarketplaceItem("Kamisah", "A-", 2500000, 325000, 50, "Modal Dagang Warung Makan"))
   }
