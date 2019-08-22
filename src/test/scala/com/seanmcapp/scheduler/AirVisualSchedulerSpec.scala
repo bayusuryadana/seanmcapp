f82cf93bc945a61793e8c38274134528daeb9c4b
Suryadana, Bayu <bayusuryadana@gmail.com>
2019-08-22 21:03:50 +0800

    fix matcher

--- src/test/scala/com/seanmcapp/scheduler/AirVisualSchedulerSpec.scala
+++ src/test/scala/com/seanmcapp/scheduler/AirVisualSchedulerSpec.scala
@@ -14,7 +14,7 @@ class AirVisualSchedulerSpec extends AsyncWordSpec with Matchers with SchedulerF
   "AirVisualScheduler should return correctly" in {
     val airVisual = new AirVisualScheduler(startTime, interval, http) with TelegramRequestBuilderMock
     val mockResponse = Source.fromResource("scheduler/airvisual_response.json").mkString
-    when(http.sendRequest(anyString())).thenReturn(mockResponse)
+    when(http.sendRequest(anyString(), any(), any(), any())).thenReturn(mockResponse)
     val expected = Map(
       AirvisualCity("Indonesia", "Jakarta", "Jakarta") -> 119,
       AirvisualCity("Indonesia", "West Java", "Bekasi") -> 119,
--- src/test/scala/com/seanmcapp/scheduler/IGrowSchedulerSpec.scala
+++ src/test/scala/com/seanmcapp/scheduler/IGrowSchedulerSpec.scala
@@ -3,7 +3,7 @@ package com.seanmcapp.scheduler
 import com.seanmcapp.SchedulerForTest
 import com.seanmcapp.mock.requestbuilder.TelegramRequestBuilderMock
 import com.seanmcapp.util.parser.decoder.IgrowData
-import org.mockito.ArgumentMatchers.anyString
+import org.mockito.ArgumentMatchers.{any, anyString}
 import org.mockito.Mockito.when
 import org.scalatest.{AsyncWordSpec, Matchers}
 
@@ -14,7 +14,7 @@ class IGrowSchedulerSpec extends AsyncWordSpec with Matchers with SchedulerForTe
   "IGrowScheduler should return correctly" in {
     val igrow = new IGrowScheduler(startTime, interval, http) with TelegramRequestBuilderMock
     val mockResponse = Source.fromResource("scheduler/igrow_response.json").mkString
-    when(http.sendRequest(anyString())).thenReturn(mockResponse)
+    when(http.sendRequest(anyString(), any(), any(), any())).thenReturn(mockResponse)
     igrow.task shouldBe List(IgrowData("Gemilang Sarea Farm Eggs Project",4000000,26,"18% per annum",3))
   }
 
