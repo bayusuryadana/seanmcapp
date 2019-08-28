dad6448768fcc3172b6521d942a602d061b85bfd
Suryadana, Bayu <bayusuryadana@gmail.com>
2019-08-28 16:16:54 +0800

    implementation

--- src/main/scala/com/seanmcapp/service/DotaService.scala
+++ src/main/scala/com/seanmcapp/service/DotaService.scala
@@ -4,7 +4,7 @@ import java.text.SimpleDateFormat
 import java.util.{Date, TimeZone}
 
 import com.seanmcapp.repository.dota.{HeroRepo, Player, PlayerRepo}
-import com.seanmcapp.util.parser.encoder.{HeroPageResponse, HomePageResponse, MatchPlayer, MatchViewModel, PlayerPageResponse, WinSummary}
+import com.seanmcapp.util.parser.encoder.{HeroPageResponse, HeroWinSummary, HomePageResponse, MatchPlayer, MatchViewModel, PlayerPageResponse, PlayerWinSummary}
 import com.seanmcapp.util.parser.decoder.{MatchResponseWithPlayer, PeerResponse}
 import com.seanmcapp.util.requestbuilder.{DotaRequestBuilder, HttpRequestBuilder}
 
@@ -25,8 +25,8 @@ class DotaService(playerRepo: PlayerRepo, heroRepo: HeroRepo, override val http:
       val matchViewModels = players.flatMap(getMatches).groupBy(_.mr.startTime).toSeq.sortBy(-_._1).map(_._2).take(10).flatMap { identicalMatches =>
         identicalMatches.headOption.map { mrwpHead =>
           val matchPlayerList = identicalMatches.map { mrwp =>
-            val heroName = heroes.find(_.id == mrwp.mr.heroId).map(_.localizedName).getOrElse("Unknown")
-            MatchPlayer(mrwp.player.personaName, heroName, s"${mrwp.mr.kills}/${mrwp.mr.deaths}/${mrwp.mr.assists}")
+            val hero = heroes.find(_.id == mrwp.mr.heroId)
+            MatchPlayer(mrwp.player, hero, mrwp.mr.kills, mrwp.mr.deaths, mrwp.mr.assists)
           }
           toMatchViewModel(mrwpHead, matchPlayerList)
         }
@@ -45,13 +45,14 @@ class DotaService(playerRepo: PlayerRepo, heroRepo: HeroRepo, override val http:
       val player = players.find(_.id == id).getOrElse(throw new Exception("Player not found"))
 
       val heroesWinSummary = getMatches(player).groupBy(_.mr.heroId).toSeq.map { case (heroId, identicalMatches) =>
-        val heroName = heroes.find(_.id == heroId).map(_.localizedName).getOrElse("Unknown")
+        val hero = heroes.find(_.id == heroId)
         val matchResponses = identicalMatches.map { mrwp =>
-          val matchPlayer = MatchPlayer(mrwp.player.personaName, heroName, s"${mrwp.mr.kills}/${mrwp.mr.deaths}/${mrwp.mr.assists}")
+          val matchPlayer = MatchPlayer(mrwp.player, hero, mrwp.mr.kills, mrwp.mr.deaths, mrwp.mr.assists)
           toMatchViewModel(mrwp, Seq(matchPlayer))
         }
-        (heroName, matchResponses)
-      }.map(toWinSummary).sortBy(-_.percentage)
+        val (win, game, percentage) = toWinSummary(matchResponses)
+        HeroWinSummary(hero, win, game, percentage)
+      }.sortBy(-_.percentage)
 
       val peers = getPeers(id).foldLeft(Seq.empty[(Player, PeerResponse)]) { (res, peer) =>
         players.find(_.id == peer.peerPlayerId) match {
@@ -61,7 +62,7 @@ class DotaService(playerRepo: PlayerRepo, heroRepo: HeroRepo, override val http:
       }
 
       val peerPlayerWinSummary = peers.map { p =>
-        WinSummary(p._1.personaName, p._2.win, p._2.games, ((p._2.win.toDouble/p._2.games) * 100).toInt / 100.0)
+        PlayerWinSummary(p._1, p._2.win, p._2.games, ((p._2.win.toDouble/p._2.games) * 100).toInt / 100.0)
       }.sortBy(-_.percentage)
 
       PlayerPageResponse(player, heroesWinSummary, peerPlayerWinSummary)
@@ -74,26 +75,26 @@ class DotaService(playerRepo: PlayerRepo, heroRepo: HeroRepo, override val http:
 
     for {
       players <- playersF
-      heroOption <- heroF
+      hero <- heroF
     } yield {
-      val hero = heroOption.getOrElse(throw new Exception("Hero not found"))
 
       val playersWinSummary = players.flatMap(getMatches).filter(_.mr.heroId == id).groupBy(_.player).toSeq.map { tup =>
         val matchResponses = tup._2.map { matchHead =>
-          val matchPlayerList = tup._2.map { m =>
-            MatchPlayer(m.player.personaName, hero.localizedName, s"${m.mr.kills}/${m.mr.deaths}/${m.mr.assists}")
+          val matchPlayerList = tup._2.map { mrwp =>
+            MatchPlayer(mrwp.player, hero, mrwp.mr.kills, mrwp.mr.deaths, mrwp.mr.assists)
           }
           toMatchViewModel(matchHead, matchPlayerList)
         }
-        (tup._1.personaName, matchResponses)
-      }.map(toWinSummary)
+        val (win, game, percentage) = toWinSummary(matchResponses)
+        PlayerWinSummary(tup._1, win, game, percentage)
+      }
       HeroPageResponse(hero, playersWinSummary)
     }
   }
 
   private def toMatchViewModel(mrwp: MatchResponseWithPlayer, players: Seq[MatchPlayer]): MatchViewModel = {
     val date = new Date(mrwp.mr.startTime.toLong * 1000L)
-    val fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm")
+    val fmt = new SimpleDateFormat("dd-MM-yyyy HH:mm")
     fmt.setTimeZone(TimeZone.getTimeZone("GMT+7"))
     val startTime = fmt.format(date.getTime)
 
@@ -108,11 +109,11 @@ class DotaService(playerRepo: PlayerRepo, heroRepo: HeroRepo, override val http:
     )
   }
 
-  private def toWinSummary(e: (String, Seq[MatchViewModel])): WinSummary = {
-    val games = e._2.size
-    val win = e._2.count(_.result == "Win")
+  private def toWinSummary(matchViewList: Seq[MatchViewModel]): (Int, Int, Double) = {
+    val games = matchViewList.size
+    val win = matchViewList.count(_.result == "Win")
     val percentage = (win.toDouble / games * 100).toInt / 100.0
-    WinSummary(e._1, win, games, percentage)
+    (win, games, percentage)
   }
 
 }
