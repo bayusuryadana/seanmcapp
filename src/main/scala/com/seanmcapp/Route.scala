4cfe3d818d1c0775e4c8409563989d40dcefb6a1
bayu suryadana <bayusuryadana@gmail.com>
2019-09-09 01:49:52 +0800

    add new route for preflighted http call

--- src/main/scala/com/seanmcapp/Route.scala
+++ src/main/scala/com/seanmcapp/Route.scala
@@ -3,6 +3,7 @@ package com.seanmcapp
 import akka.actor.ActorSystem
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.model.HttpHeader
+import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.server.Directives
 import akka.stream.Materializer
 import com.seanmcapp.repository.instagram.Photo
@@ -32,7 +33,10 @@ class Route(implicit system: ActorSystem, mat: Materializer, ec: ExecutionContex
     get(path("dota" / "hero" /  Remaining)(id => complete(dotaAPI.hero(id.toInt).map(_.toJson)))),
 
     // wallet
-    get((path("wallet") & headerValue(extractHeader))(secretKey => complete(walletAPI.getAll(secretKey).map(_.toJson)))),
+    options(path("wallet")(respondWithHeaders(RawHeader("Access-Control-Allow-Origin", "*"), RawHeader("Access-Control-Allow-Headers", "*"))(complete("")))),
+    get((path("wallet") & headerValue(extractHeader)) { secretKey =>
+      respondWithHeaders(RawHeader("Access-Control-Allow-Origin", "*"))(complete(walletAPI.getAll(secretKey).map(_.toJson)))
+    }),
     post((path("wallet") & headerValue(extractHeader) & entity(as[JsValue]))((secretKey, payload) => complete(walletAPI.insert(payload)(secretKey).map(_.toJson)))),
     put((path("wallet") & headerValue(extractHeader) & entity(as[JsValue]))((secretKey, payload) => complete(walletAPI.update(payload)(secretKey).map(_.toJson)))),
     delete((path("wallet" / Remaining) & headerValue(extractHeader))((id, secretKey) => complete(walletAPI.delete(id.toInt)(secretKey).map(_.toJson)))),
