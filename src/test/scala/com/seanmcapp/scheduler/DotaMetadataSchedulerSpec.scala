8b4c2ad98f71e6562eb52d74092ddef67c1190f7
bayu suryadana <bayusuryadana@gmail.com>
2020-05-08 17:55:08 +0800

    fix prev commit test

--- src/test/scala/com/seanmcapp/scheduler/DotaMetadataSchedulerSpec.scala
+++ src/test/scala/com/seanmcapp/scheduler/DotaMetadataSchedulerSpec.scala
@@ -27,10 +27,7 @@ class DotaMetadataSchedulerSpec extends AsyncWordSpec with Matchers with Schedul
 
     val expectedPlayerResponse = List(
       PlayerResponse(ProfileResponse("hnymnky", "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/d1/d145e4465a020a67d8bcdefb362dae8019d2af4f_full.jpg"),None),
-      PlayerResponse(ProfileResponse("travengers", "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/c0/c03e59d51d93b3df4c6ea5f49a246ef1ed2836e6_full.jpg"),Some(43)),
-      PlayerResponse(ProfileResponse("SeanmcrayZ", "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/88/88b1333d6f78f9426fb51141c6d5fa8254b6e798_full.jpg"),Some(54)),
-      PlayerResponse(ProfileResponse("OMEGALUL", "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/58/5899046336c4a14358467c331a9de24f6daded9f_full.jpg"),Some(67)),
-      PlayerResponse(ProfileResponse("lightzard", "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/d3/d36ccfdfde5f66d6a109e2252d94337471277cf4_full.jpg"),None)
+      PlayerResponse(ProfileResponse("OMEGALUL", "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/58/5899046336c4a14358467c331a9de24f6daded9f_full.jpg"),Some(67))
     )
     val expectedHeroResponse = List(Hero(1, "Anti-Mage", "agi", "Melee", "Carry,Escape,Nuker", "antimage_full.png", "antimage_icon.png", ""))
 
