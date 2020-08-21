d8fa1e7065650361fe5324f28bb91fe9aa8acebd
bayu suryadana <bayusuryadana@gmail.com>
2020-08-22 00:40:06 +0800

    fix encode output

--- src/main/scala/com/seanmcapp/DecoderProvider.scala
+++ src/main/scala/com/seanmcapp/DecoderProvider.scala
@@ -1,6 +1,6 @@
 package com.seanmcapp
 
-import io.circe.Decoder
+import io.circe.{Decoder, Json, Printer}
 import io.circe.generic.AutoDerivation
 import io.circe.parser.decode
 
@@ -20,5 +20,9 @@ package object external extends AutoDerivation {
         case Right(res) => res
         case Left(e) => throw new Exception(s"Unable to deserialize json response\n===== Exception =====\n$e\n\n===== INPUT =====\n$input")
       }
+    }
+
+    implicit class Encoder(json: Json) {
+      def encode: String = json.printWith(Printer.noSpacesSortKeys)
     }
 }
