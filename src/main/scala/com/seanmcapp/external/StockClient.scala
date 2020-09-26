23ab5f844dfd8e53e426fdf892ec7e48dd2636dc
bayu suryadana <bayusuryadana@gmail.com>
2020-09-26 22:44:12 +0800

    cache stock

--- src/main/scala/com/seanmcapp/external/StockClient.scala
+++ src/main/scala/com/seanmcapp/external/StockClient.scala
@@ -1,16 +1,30 @@
 package com.seanmcapp.external
 
-class StockClient(http: HttpRequestClient) {
+import java.util.concurrent.TimeUnit
+
+import com.seanmcapp.util.MemoryCache
+import scalacache.Cache
+import scalacache.memoization.memoizeSync
+import scalacache.modes.sync._
+
+import scala.concurrent.duration.Duration
+
+class StockClient(http: HttpRequestClient) extends MemoryCache {
+
+  implicit val stockCache: Cache[StockResponse] = createCache[StockResponse]
+  private val duration = Duration(15, TimeUnit.MINUTES)
 
   def getStockResult(stockIdList: Seq[String]): StockResponse = {
-    val url = s"https://bloomberg-market-and-financial-news.p.rapidapi.com/market/get-chart?" +
-      s"id=${stockIdList.foldLeft("")((s,c) => s"$s$c:IJ,").dropRight(1)}&interval=d1"
-    val headers = HeaderMap(Map(
-      "x-rapidapi-host" -> "bloomberg-market-and-financial-news.p.rapidapi.com",
-      "x-rapidapi-key" -> "1917b0cf26msh7824c7e68f8c4cap16c0a7jsn3583cd703049"
-    ))
-    val response = http.sendRequest(url, headers = Some(headers))
-    decode[StockResponse](response)
+    memoizeSync(Some(duration)) {
+      val url = s"https://bloomberg-market-and-financial-news.p.rapidapi.com/market/get-chart?" +
+        s"id=${stockIdList.foldLeft("")((s, c) => s"$s$c:IJ,").dropRight(1)}&interval=d1"
+      val headers = HeaderMap(Map(
+        "x-rapidapi-host" -> "bloomberg-market-and-financial-news.p.rapidapi.com",
+        "x-rapidapi-key" -> "1917b0cf26msh7824c7e68f8c4cap16c0a7jsn3583cd703049"
+      ))
+      val response = http.sendRequest(url, headers = Some(headers))
+      decode[StockResponse](response)
+    }
   }
 
 }
