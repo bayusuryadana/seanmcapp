2acf273816ed4a8576f7c460cfba00db9721e9d6
bayu suryadana <bayusuryadana@gmail.com>
2020-10-21 20:57:25 +0800

    add more test to wallet service

--- src/main/scala/com/seanmcapp/service/WalletService.scala
+++ src/main/scala/com/seanmcapp/service/WalletService.scala
@@ -82,20 +82,7 @@ class WalletService(walletRepo: WalletRepo, amarthaService: AmarthaService) {
     val prevDate = adjustDate(requestDate-1).toString
 
     val requestedMonth = requestDate % 100
-    val monthString = requestedMonth match {
-      case 1 => "January"
-      case 2 => "February"
-      case 3 => "March"
-      case 4 => "April"
-      case 5 => "May"
-      case 6 => "June"
-      case 7 => "July"
-      case 8 => "August"
-      case 9 => "September"
-      case 10 => "October"
-      case 11 => "November"
-      case 12 => "December"
-    }
+    val monthString = getMonth(requestedMonth)
     val yearString = (requestDate / 100).toString
     val cmsData = CMSData(monthString, yearString, nextDate, prevDate)
 
@@ -189,6 +176,23 @@ class WalletService(walletRepo: WalletRepo, amarthaService: AmarthaService) {
     def formatNumber: String = {
       val formatter = NumberFormat.getIntegerInstance
       formatter.format(in)
+    }
+  }
+
+  private[service] def getMonth(i: Int): String = {
+    i match {
+      case 1 => "January"
+      case 2 => "February"
+      case 3 => "March"
+      case 4 => "April"
+      case 5 => "May"
+      case 6 => "June"
+      case 7 => "July"
+      case 8 => "August"
+      case 9 => "September"
+      case 10 => "October"
+      case 11 => "November"
+      case 12 => "December"
     }
   }
 
--- src/test/scala/com/seanmcapp/service/WalletServiceSpec.scala
+++ src/test/scala/com/seanmcapp/service/WalletServiceSpec.scala
@@ -63,4 +63,10 @@ class WalletServiceSpec extends AsyncWordSpec with Matchers {
     res shouldBe 1
   }
 
+  "getMonth should return month name respectively" in {
+    val i = List(1,2,3,4,5,6,7,8,9,10,11,12)
+    val expected = List("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+    i.map(walletService.getMonth) shouldBe expected
+  }
+
 }
