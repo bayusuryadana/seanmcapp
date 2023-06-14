c9dca539b407be8f6389b3f9de75cbd35ba18a50
Suryadana, Bayu <bayusuryadana@gmail.com>
2023-06-14 18:06:27 +0800

    fix test:compile

--- src/test/scala/com/seanmcapp/service/MamenServiceSpec.scala
+++ src/test/scala/com/seanmcapp/service/MamenServiceSpec.scala
@@ -12,10 +12,10 @@ class MamenServiceSpec extends AsyncWordSpec with Matchers {
   
   val stallRepo = Mockito.mock(classOf[StallRepo])
   val stallRepoData = Seq(
-    Stall(1, "Ayam Suharti", "Rumah makan jual ayam goreng kremes", "Jl. Kapten tendean No.23, Jakarta Selatan", Cities.Jakarta, -6.23, 106.67, "https://youtube.com", "https://goo.gl/maps/dxcXghzHrhn96AZy6"),
-    Stall(2, "Rm. Sederhana", "Masakan bundo ajo", "Jl. Balai Pustaka No.1, Jakarta Timur", Cities.Jakarta, -6.23, 106.67, "https://youtube.com", "https://goo.gl/maps/dxcXghzHrhn96AZy6"),
-    Stall(3, "Warteg Bahari", "Masakan mahasiswa sehari-hari", "Jl. Desa Putra No.5 Jakarta Barat", Cities.Surabaya, -8.23, 110.67, "https://youtube.com", "https://goo.gl/maps/dxcXghzHrhn96AZy6"),
-    Stall(4, "Sate Madura Pak Kumis", "Gue punya ayam nih. Bakar rumah lu yuk", "Jl. Mendoan selatan No.46 Jakarta Utara", Cities.Surabaya, -8.23, 110.67, "https://youtube.com", "https://goo.gl/maps/dxcXghzHrhn96AZy6")
+    Stall(1, "Ayam Suharti", "7JPF+GX2, Ps. Gn. Sitoli, Kec. Gunungsitoli, Kota Gunungsitoli, Sumatera Utara", Cities.Jakarta, "https://youtube.com", "https://goo.gl/maps/dxcXghzHrhn96AZy6", Some(-6.23), Some(106.67), None),
+    Stall(2, "Rm. Sederhana", "MRQJ+WR9, Aek Tolang, Kec. Pandan, Kabupaten Tapanuli Tengah, Sumatera Utara 22611", Cities.Jakarta, "https://youtube.com", "https://goo.gl/maps/dxcXghzHrhn96AZy6", Some(-6.23), Some(106.67), None),
+    Stall(3, "Warteg Bahari", "39VX+XG Gunung Sarik, Padang City, West Sumatra, Indonesia", Cities.Surabaya, "https://youtube.com", "https://goo.gl/maps/dxcXghzHrhn96AZy6", Some(-8.23), Some(110.67), None),
+    Stall(4, "Sate Madura Pak Kumis", "PJM8+72 Limbukan, Payakumbuh City, West Sumatra, Indonesia", Cities.Surabaya, "https://youtube.com", "https://goo.gl/maps/dxcXghzHrhn96AZy6", Some(-8.23), Some(110.67), None)
   )
   when(stallRepo.getAll).thenReturn(Future.successful(stallRepoData))
   
